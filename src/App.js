e60b067f3e7d2eead36f7748c2465686b60c639f
Bashir Ahamed <ahamedb1991@gmail.com>
2019-03-16 06:28:38 -0400

    address view

--- src/App.js
+++ src/App.js
@@ -3,6 +3,57 @@ import logo from './logo.jpg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    addressBook: [
+      {
+        FirstName: "Cathy" ,
+        LastName: "Pierce",
+        Birthday: "9/14/1996",
+        Telephone: "200-910-8132"
+      },
+      {
+        FirstName: "Alfonso",
+        LastName: "Cooley",
+        Birthday: "8/10/1973",
+        Telephone: "200-657-9362"
+      },
+      {
+        FirstName: "Victor",
+        LastName: "Gordon",
+        Birthday: "8/3/1970",
+        Telephone: "200-661-9407"
+      },
+      {
+        FirstName: "Colleen",
+        LastName: "Wright",
+        Birthday: "10/28/1967",
+        Telephone: "200-250-7949"
+      },
+      {
+        FirstName: "James",
+        LastName: "Johnston",
+        Birthday: "5/11/1972",
+        Telephone: "200-645-3176"
+      },
+      {
+        FirstName: "Anna",
+        LastName: "Reyes",
+        Birthday: "9/10/1975",
+        Telephone: "200-707-8670"
+      }
+    ],
+
+    name: '',
+    dob: '',
+    contact: ''
+  }
+  
+  displayAddressBook = (props) =>{
+      return props.Book.map( (item) =>
+        <div>{item.FirstName}</div>
+      );
+  }
+
   render() {
     return (
       <div className="App">
@@ -15,20 +66,22 @@ class App extends Component {
               Your address book...
             </h2>
             <p>
-              Read, Add, Delete, or Modify your Address Book
+              Search or Modify Address Book
             </p>
+            
           </div>
         </header>
-        <a
-          className="App-link"
-          href=""
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        Add New Contact
-        </a>
+        <button className="addContact">Add New Contact</button>
+        {/* <displayAddressBook Book = {this.state.addressBook}/> */}
+        <div className="view">{this.state.addressBook.map(person =>
+          <div className="person">
+            <div>{person.FirstName}, {person.LastName}</div>
+            <div>{person.Birthday}</div>
+            <div>{person.Telephone}</div>
+          </div>
+          )}
+        </div>
       </div>
-      
     );
   }
 }
