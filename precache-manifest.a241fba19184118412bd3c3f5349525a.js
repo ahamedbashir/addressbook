5423a547d9b0f1fd6951f5c5065e52d4c7b734ce
Bashir Ahamed <ahamedb1991@gmail.com>
2019-03-19 23:55:30 -0400

    Updates

---
+++ precache-manifest.a241fba19184118412bd3c3f5349525a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "baa34d1c172e8c60217e",
+    "url": "/addressbook/static/js/runtime~main.8ba80d7c.js"
+  },
+  {
+    "revision": "8acf4a88a0d333d7e40d",
+    "url": "/addressbook/static/js/main.3f4ffc47.chunk.js"
+  },
+  {
+    "revision": "c3f137769478d15befd7",
+    "url": "/addressbook/static/js/2.696fc729.chunk.js"
+  },
+  {
+    "revision": "8acf4a88a0d333d7e40d",
+    "url": "/addressbook/static/css/main.77618554.chunk.css"
+  },
+  {
+    "revision": "d4bb70ec31ebed2be6dedddd8ade5db4",
+    "url": "/addressbook/index.html"
+  }
+];
